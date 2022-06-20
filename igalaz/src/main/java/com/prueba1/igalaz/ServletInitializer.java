235fbae8a0f25bc0fa194ffe9e41542199c69a9f
NachoGalaz <igalazf@gmail.com>
2022-06-20 15:20:01 -0400

    prueba1

---
+++ igalaz/src/main/java/com/prueba1/igalaz/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.prueba1.igalaz;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(IgalazApplication.class);
+	}
+
+}
---
+++ igalaz/src/test/java/com/prueba1/igalaz/IgalazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba1.igalaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgalazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
